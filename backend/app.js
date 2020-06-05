bce0583197a3838cd001bf539827686d2b8e370d
dglabs <reach.dglabs@gmail.com>
2020-06-05 23:33:01 +0530

    deployment to heroku

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,29 @@
 const express = require('express');
+const path = require('path');
 
 var app = express();  
 
 app.use("/api/visits",(req,res,next) => {
-    const visits = [
+    const visits = [ 
         {
             visit_id:"12",
             visitor_name:"Mr.kkk"
         },
         {
             visit_id:"14",
-            visitor_name:"Mr.mkk"
+            visitor_name:"Mr.mkk" 
         }
     ];
     res.json({
-        message:"Success",
+        message:"Success", 
         visits: visits
     });
 });
-
-
-module.exports = app;
+app.use('/', express.static(path.join(__dirname,"angular")));
+app.use((req,res,next) => {
+    res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+    res.sendFile(path.join(__dirname,"angular","index.html"));
+});
+ 
+module.exports = app;    
